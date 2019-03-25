ac6d995921d1832c9b326cefc266049f831fe55f
bq-ashok <31696178+bq-ashok@users.noreply.github.com>
2019-03-25 11:46:28 +0530

    Add files via upload

---
+++ src/app/Directives/mouse-event.directive.spec.ts
@@ -0,0 +1,7 @@
+import { MouseEventDirective } from './mouse-event.directive';
+describe('MouseEventDirective', () => {
+  it('should create an instance', () => {
+    const directive = MouseEventDirective;
+    expect(directive).toBeTruthy();
+  });
+});
