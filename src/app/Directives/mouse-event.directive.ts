27f9646395b2c0877bc8fc0e1c6f990daff97c4c
bq-ashok <31696178+bq-ashok@users.noreply.github.com>
2019-03-25 11:45:30 +0530

    Add files via upload

---
+++ src/app/Directives/mouse-event.directive.ts
@@ -0,0 +1,24 @@
+import { Directive, ElementRef, HostListener, Input } from '@angular/core';
+
+@Directive({
+  selector: '[appMouseEvent]'
+})
+export class MouseEventDirective {
+  
+
+  constructor(private el: ElementRef) { }
+  @Input() defaultColor: string ;
+  @Input() highlightColor: string;
+
+  @HostListener('mouseenter')  onMouseEnter() {
+    this.highLight(this.highlightColor || this.defaultColor || 'orange');
+  }
+
+  @HostListener('mouseleave') onMouseLeave() {
+    this.highLight(null);
+
+  }
+  private highLight(color) {
+    this.el.nativeElement.style.backgroundColor= color;
+  }
+}
