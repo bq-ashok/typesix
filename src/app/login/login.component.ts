1be94a3679dcdca621dd526ca5c67c91ac3ef5aa
bq-ashok <31696178+bq-ashok@users.noreply.github.com>
2019-03-25 11:55:33 +0530

    Update login.component.ts

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -9,34 +9,29 @@ import {Router} from '@angular/router';
 })
 export class LoginComponent implements OnInit {
 
-  submitted: boolean = false;
-  invalidLogin: boolean = false;
+  submitted = false;
+  invalidLogin = false;
   model: object = {};
-  datas:  object  = [];
+  datas: object  = [];
   constructor(private router: Router) {
     this.datas = [
-      { "name":"John", "age":30, "car":null },
-      { "name":"ashok", "age":30, "car":null },
-      { "name":"kumar", "age":20, "car":"bmw" }
+      { name: 'John', age: 30, car: null },
+      { name: 'ashok', age: 30, car: null },
+      { name: 'kumar', age: 20, car: 'bmw' }
     ];
    }
 
   onSubmit(f) {
     this.submitted = true;
     console.log(JSON.stringify(this.model), this.datas);
-    this.router.navigate(['user'], { state: this.datas});
-    // if (this.loginForm.invalid) {
-    //   return;
-    // }
-    // console.log(this);
-    // if(this.loginForm.controls.email.value == 'dhiraj@gmail.com' && this.loginForm.controls.password.value == 'password') {
-    //     this.router.navigate(['list-user']);
-    // }else {
-    //   this.invalidLogin = true;
-    // }
+    this.router.navigate(['user'], { queryParams: { page: '55525' }} );
+  }
+  ngOnInit() {
+    console.log('init methods get called');
   }
 
-  ngOnInit() {
-  }
+  changedExtraHandler(changes) {
+    console.log(changes);
+}
 
 }
