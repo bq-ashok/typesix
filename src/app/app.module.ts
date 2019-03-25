af211cd8cb3c67251fbda6cc35c93c21a47815e5
bq-ashok <31696178+bq-ashok@users.noreply.github.com>
2019-03-25 11:51:18 +0530

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,12 +9,16 @@ import { LoginComponent } from './login/login.component';
 import { UserComponent } from './user/user.component';
 
 import { HttpConfigInterceptor } from './interceptor/httpconfig.interceptor';
+import {UserService } from './services/user.service';
+
+import { MouseEventDirective } from './directives/mouse-event.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    UserComponent
+    UserComponent,
+    MouseEventDirective
   ],
   imports: [
     BrowserModule,
@@ -22,8 +26,9 @@ import { HttpConfigInterceptor } from './interceptor/httpconfig.interceptor';
     FormsModule,
     HttpClientModule
   ],
-  providers: [ 
-    { provide : HTTP_INTERCEPTORS, useClass: HttpConfigInterceptor, multi: true}
+  providers: [
+    { provide : HTTP_INTERCEPTORS, useClass: HttpConfigInterceptor, multi: true},
+    UserService
   ],
   bootstrap: [AppComponent]
 })
