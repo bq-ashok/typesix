60e8eb75d1b69d5d68440e49890806b63c4d09ae
bq-ashok <31696178+bq-ashok@users.noreply.github.com>
2019-05-28 15:14:50 +0530

    object filter pipes added

---
+++ src/app/pipes/filter-text.pipe.ts
@@ -0,0 +1,29 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'filterText'
+})
+export class FilterTextPipe implements PipeTransform {
+
+  transform(items : any[], searchText: string): any {
+    console.log('sssssssssssssdffff', items, searchText)
+    if(!items) return [];
+    if(!searchText) return items;   
+    // return items.filter( it => { it.login.toLowerCase().includes(searchText) }); // list filtes    
+    let filteredItems = [];
+    searchText = searchText.toLocaleLowerCase();
+    console.log('searchText', searchText);
+    items.forEach(item => {
+        if (item.login.toLowerCase().includes(searchText)) {
+          filteredItems.push(item);
+        }
+    })
+    return filteredItems;
+ 
+
+  }
+
+}
+
+
+// https://www.reddit.com/r/Angular2/comments/7iycy8/filtering_an_array_of_objects/
